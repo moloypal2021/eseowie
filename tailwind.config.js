7bedbe639c44fda8b5205102a2d8050c45996479
Moloy Pal <77716576+moloypal2021@users.noreply.github.com>
2021-03-27 13:35:46 +0600

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: "#4D0ADB",
+        yellow: "#C0E914",
+        "paragraph-alt": "#A6A6A6",
+        paragraph: "#656565",
+      },
+    },
   },
   variants: {
     extend: {},
